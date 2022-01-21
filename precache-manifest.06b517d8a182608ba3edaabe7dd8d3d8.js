30fa3d10ce4793b0114f4fa942585baab661122a
FelippeAlves <felippealves17@gmail.com>
2022-01-21 17:22:35 -0300

    Updates

---
+++ precache-manifest.06b517d8a182608ba3edaabe7dd8d3d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dab98f6856314f1b32a5e873109f715",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "0db17f0f535fbc821b8b",
+    "url": "/react-app/static/css/main.11a39a0a.chunk.css"
+  },
+  {
+    "revision": "10ae0603604359519bfb",
+    "url": "/react-app/static/js/2.50c414a1.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-app/static/js/2.50c414a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db17f0f535fbc821b8b",
+    "url": "/react-app/static/js/main.d6341306.chunk.js"
+  },
+  {
+    "revision": "a0869fd3ed4d6a5b8e7c",
+    "url": "/react-app/static/js/runtime-main.d74d1e89.js"
+  }
+]);
